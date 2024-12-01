6f8ffa05163b1eff27c7f47daa42c7cb838325be
Geetesh Kumbalkar <geetesh.kumbalkar@wittygreens.com>
2024-12-02 03:38:32 +0530

    ok

--- src/main/java/com/spring/jwt/JwtWithSpringSecurityApplication.java
+++ src/main/java/com/spring/jwt/JwtWithSpringSecurityApplication.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class JwtWithSpringSecurityApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(JwtWithSpringSecurityApplication.class, args);
-
 		System.out.println("\n\n");
-
+		System.err.println("PORT : localhost8080");
+		System.err.println("documentation : "+"http://localhost:8080/swagger-ui/index.html#/");
 		System.err.println("  *****    *******  *******       *****   *******    *****    ******   *******" );
 		System.err.println(" *     *   *      *    *         *           *      *     *   *     *     *   " );
 		System.err.println("*       *  *      *    *         *           *     *       *  *     *     *   " );
@@ -19,6 +22,6 @@ public class JwtWithSpringSecurityApplication {
 		System.err.println("*********  *           *               *     *     *********  *   *       *   " );
 		System.err.println("*       *  *           *               *     *     *       *  *    *      *   " );
 		System.err.println("*       *  *        *******       *****      *     *       *  *     *     *   " );
-	}
+		}
 
 }
--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -80,6 +80,19 @@ public class AppConfig {
                 .formLogin().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/account/**").permitAll()
+                .requestMatchers(
+                        "/api/v1/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v*/a*-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html"
+                ).permitAll()
                 .requestMatchers("/api/EA/**").permitAll()
                 .requestMatchers("/api/employees/**").permitAll()
                 .requestMatchers("/api/ESCC/**").permitAll()
